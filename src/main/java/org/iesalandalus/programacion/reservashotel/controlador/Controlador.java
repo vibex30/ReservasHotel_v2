bec7bfd3e88992e69c2a09d3fdc8488ef2afa778
Andrés Rubio del Río <andres.rubio@iesalandalus.org>
2024-01-31 14:03:21 +0100

    Delete src/main/java/org/iesalandalus/programacion/reservashotel/controlador directory

--- src/main/java/org/iesalandalus/programacion/reservashotel/controlador/Controlador.java
+++
@@ -1,9 +0,0 @@
-package org.iesalandalus.programacion.reservashotel.controlador;
-
-import javax.naming.OperationNotSupportedException;
-import java.time.LocalDateTime;
-import java.util.List;
-
-public class Controlador {
-
-}
